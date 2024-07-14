32c8c072fc4d9f679b76562155a02e1b680bff07
kushalchh07 <kushalchhetri54@gmail.com>
2024-07-14 13:27:38 +0545

    feat: Colors added

--- lib/constants/color/color.dart
+++ lib/constants/color/color.dart
@@ -50,7 +50,22 @@ Color myInputBorderColor = const Color.fromARGB(255, 197, 197, 197);
 
 Color myMoneyColor = const Color.fromARGB(255, 24, 162, 92);
 
-const Color primaryColor = Color.fromARGB(255, 97, 97, 97);
+const Color primaryColor = Color.fromARGB(255, 71, 150, 189);
+const Color primaryColorjustDark = Color.fromARGB(255, 65, 149, 180);
+const Color primaryColorDark = Color.fromARGB(255, 61, 151, 168);
+const Color primaryColorVeryDark = Color.fromARGB(255, 47, 117, 131);
+const Color primaryColorJustLight = Color.fromARGB(255, 124, 177, 208);
+
+const Color primaryColorLight = Color.fromARGB(255, 143, 183, 214);
+const Color primaryColorVeryLight = Color.fromARGB(255, 173, 199, 225);
+const Color background = Color.fromARGB(255, 243, 249, 251);
+const Color greyVeryDark = Color.fromARGB(255, 18, 46, 64);
+const Color greyDark = Color.fromARGB(255, 73, 93, 105);
+const Color grey = Color.fromARGB(255, 175, 198, 207);
+const Color greyLight = Color.fromARGB(255, 231, 239, 243);
+const Color greyVeryLight = Color.fromARGB(255, 244, 249, 250);
+const Color paperBackgrounfd = Color.fromARGB(255, 255, 255, 254);
+
 const Color secondaryColor = Color.fromARGB(255, 255, 145, 1);
 const Color myButtonColor = Colors.black;
 const Color redColor = Colors.red;
